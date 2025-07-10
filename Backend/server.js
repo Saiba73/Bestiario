793d726d10cd088a4c50d90273e4b2c28aaae306
Saiba73 <jesus.herreradegadillo.30350@gmail.com>
2025-07-10 14:50:02 -0600

    Agrega parte de la implementacion para que el frontend  y backend puedan ser subidos al servidor

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import { conectarDB } from "./config/db.js";
 import monstruoRutas from "./rutas/monstruo.rutas.js"
 import usuarioRutas from "./rutas/usuario.rutas.js"
@@ -11,11 +12,21 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 app.use(express.json()); // permite aceptar datos en formato json en la petiicon del usuario
 
 
 app.use("/api/auth", usuarioRutas);
-app.use("/api/monstruos", monstruoRutas)
+app.use("/api/monstruos", monstruoRutas);
+
+if(process.env.NODE_ENV === "production"){
+
+    app.use(express.static(path.join(__dirname, "/Frontend")));
+
+    app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "html", "index.html"));
+    });
+}
 
 //inicia el servidor
 app.listen(5000, () => {
