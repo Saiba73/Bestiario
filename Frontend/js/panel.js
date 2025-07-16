f366b3a98e8539e0a8cecea0dcfe551796b87e3e
unknown <Ivan0076>
2025-07-16 17:03:28 -0600

    Ajuste de html para coincidir con los cambios css, intento 2

--- Frontend/js/panel.js
+++ Frontend/js/panel.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', async () => {
           <button class="panel__delete-button" data-id="${m._id}">Borrar</button>
         </div>
       `;
-      // lista.insertBefore(item, addBtn);
+      lista.appendChild(item);
       clases.add(m.CLASSIFICACION);
     });
 
